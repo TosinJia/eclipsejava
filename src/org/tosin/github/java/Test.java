6707f6d9dd867653773215d802c6ee5d22a79f45
TosinJia <TosinJia@foxmail.com>
2016-04-14 08:56:51 +0800

    Update Test.java

--- src/org/tosin/github/java/Test.java
+++ src/org/tosin/github/java/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		System.out.println("elipse集成github");
 		System.out.println("更新文件");
 		System.out.println("clone a repository and update file");
+		System.out.println("在github页面中 update file");
 	}
 
 }
