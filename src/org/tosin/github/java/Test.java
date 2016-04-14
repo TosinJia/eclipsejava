a03606b78f5f3870e239f0255801a02a61208b83
TosinJia <TosinJia@foxmail.com>
2016-04-14 09:03:20 +0800

    在github页面中第二次修改文件
    在github页面中第二次修改文件，详细描述

--- src/org/tosin/github/java/Test.java
+++ src/org/tosin/github/java/Test.java
@@ -8,6 +8,7 @@ public class Test {
 		System.out.println("更新文件");
 		System.out.println("clone a repository and update file");
 		System.out.println("在github页面中 update file");
+		System.out.println("在github页面中第二次修改文件");
 	}
 
 }
